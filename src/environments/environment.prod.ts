cbc70d57d8e3145fd8a04ac8ca4dbae75fff44a3
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2021-09-21 12:15:16 -0500

    fix enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,15 @@
 export const environment = {
-  production: true,
-  // server:'https://system.novicompu.com/api/',
+  // production: true,
+  // mapbox_key: 'pk.eyJ1IjoiZmVybmFuZG8xOTkxIiwiYSI6ImNrOGRlcHF2czBxd28zbW5wa3hsaTZnaWcifQ.g1IjAr-9rd65D5W93ftlew',
+  // weather_key: '49a2c8b7cceb6f9a5b4f67b25975bb49',
+  // img_not_default: 'https://mec-s1-p.mlstatic.com/935743-MEC44870123773_022021-O.jpg',
+  // server_vtex_file: 'https://novicompu.vteximg.com.br/arquivos/',
+  // user_photo_default: 'assets/img/profile-user.svg'
+  production: false,
   server: 'https://system.novicompu.com/api/',
   server_img: 'https://system.novicompu.com/',
-  domain_serve: 'system.novicompu.com',
-  keySocket: '1564856898',
+  keySocket: 'XLuoRgESQWL3lgvbnJh11R9vmfxT1Gz8xQ2m0PXJl1hASyIEOe',
+  domain_serve: 'ws.system.novicompu.com',
   mapbox_key: 'pk.eyJ1IjoiZmVybmFuZG8xOTkxIiwiYSI6ImNrOGRlcHF2czBxd28zbW5wa3hsaTZnaWcifQ.g1IjAr-9rd65D5W93ftlew',
   weather_key: '49a2c8b7cceb6f9a5b4f67b25975bb49',
   img_not_default: 'https://mec-s1-p.mlstatic.com/935743-MEC44870123773_022021-O.jpg',
@@ -12,4 +17,3 @@ export const environment = {
   user_photo_default: 'assets/img/profile-user.svg'
 };
 
-// server:'http://sistema-codificacion.test/api/'
