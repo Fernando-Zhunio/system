a301d1b91722b4d98624963f173772d78194af76
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2021-09-21 13:04:50 -0500

    fix enviroment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
-  // production: true,
-  // mapbox_key: 'pk.eyJ1IjoiZmVybmFuZG8xOTkxIiwiYSI6ImNrOGRlcHF2czBxd28zbW5wa3hsaTZnaWcifQ.g1IjAr-9rd65D5W93ftlew',
-  // weather_key: '49a2c8b7cceb6f9a5b4f67b25975bb49',
-  // img_not_default: 'https://mec-s1-p.mlstatic.com/935743-MEC44870123773_022021-O.jpg',
-  // server_vtex_file: 'https://novicompu.vteximg.com.br/arquivos/',
-  // user_photo_default: 'assets/img/profile-user.svg'
-  production: false,
+  production: true,
   server: 'https://system.novicompu.com/api/',
   server_img: 'https://system.novicompu.com/',
   keySocket: 'XLuoRgESQWL3lgvbnJh11R9vmfxT1Gz8xQ2m0PXJl1hASyIEOe',
