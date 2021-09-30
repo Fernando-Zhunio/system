2925e9ec8d3513117c4fdf7fba541dff6806f611
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2021-09-30 16:13:38 -0500

    add new vapid public key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   keySocket: 'XLuoRgESQWL3lgvbnJh11R9vmfxT1Gz8xQ2m0PXJl1hASyIEOe',
   portSocket: 80,
   domain_serve: 'ws.system.novicompu.com',
-  VAPID_PUBLIC_KEY: 'BLIsyYHn1cyG5d4LElIci-pFXe4W6NVTuRrSXp1G9Uq3Qo4HqBQcTDYB3AAbifRjeB5g3wOHS-mdb8a10n2Hmjg',
+  VAPID_PUBLIC_KEY: 'BC1jWw641L4t0iRt5GsFA6dhakSBE6DjxAo5QncGwrwPnY5VJ4H3ED6NfyetKls10gzQkZJEIewokdylQfzqZHY',
   mapbox_key: 'pk.eyJ1IjoiZmVybmFuZG8xOTkxIiwiYSI6ImNrOGRlcHF2czBxd28zbW5wa3hsaTZnaWcifQ.g1IjAr-9rd65D5W93ftlew',
   weather_key: '49a2c8b7cceb6f9a5b4f67b25975bb49',
   img_not_default: 'https://mec-s1-p.mlstatic.com/935743-MEC44870123773_022021-O.jpg',
