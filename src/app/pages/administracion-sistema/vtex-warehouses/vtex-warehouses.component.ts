9dea6d33be9c06b337b5f5b5bd8b28bd7eec55f6
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2021-10-04 17:37:10 -0500

    stable

--- src/app/pages/administracion-sistema/vtex-warehouses/vtex-warehouses.component.ts
+++ src/app/pages/administracion-sistema/vtex-warehouses/vtex-warehouses.component.ts
@@ -13,24 +13,24 @@ import { StandartSearchService } from '../../../services/standart-search.service
 })
 export class VtexWarehousesComponent  extends CtableAndPaginator<any> implements OnInit {
 
-  constructor(public activated_route: ActivatedRoute, public s_standart: StandartSearchService, public snack_bar: MatSnackBar) {
-    super();
-   }
-  wordMain: string = 'Cuenta Ml';
-  urlDelete: string = 'admin/roles/';
-  displayedColumns: string[] = [
-    'id',
-    'user_id',
-    // "guard_name",
-    'name',
-    'city',
-    'company',
-    'date_created',
-    'date_update',
-    'acciones',
-  ];
+  // constructor(public activated_route: ActivatedRoute, public s_standart: StandartSearchService, public snack_bar: MatSnackBar) {
+  //   super();
+  //  }
+  // wordMain: string = 'Cuenta Ml';
+  // urlDelete: string = 'admin/roles/';
+  // displayedColumns: string[] = [
+  //   'id',
+  //   'user_id',
+  //   // "guard_name",
+  //   'name',
+  //   'city',
+  //   'company',
+  //   'date_created',
+  //   'date_update',
+  //   'acciones',
+  // ];
 
-  urlData: string = 'admin/vtex/1/warehouses';
+  // urlData: string = 'admin/vtex/1/warehouses';
 
   @ViewChild(HeaderSearchComponent) headerComponent: HeaderSearchComponent;
   permissions: IpermissionStandart;
