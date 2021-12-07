bfc856ca1fa30ae578b827f92e43f39a23f3c1a9
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2021-12-07 11:02:23 -0500

    remove chartng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { AppRoutingModule } from './app.routing';
 // Import 3rd party components
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ChartsModule } from 'ng2-charts';
+// import { ChartsModule } from 'ng2-charts';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
@@ -120,7 +120,7 @@ registerLocaleData(localeEs, 'es');
     PerfectScrollbarModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
-    ChartsModule,
+    // ChartsModule,
     ReactiveFormsModule,
     HttpClientModule,
     // ProductoModule,
