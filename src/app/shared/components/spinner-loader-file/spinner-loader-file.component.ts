d48fe63da30c96728889986d8ad1d450a1abfa0f
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-10-21 18:31:34 -0500

    h

--- src/app/shared/components/spinner-loader-file/spinner-loader-file.component.ts
+++ src/app/shared/components/spinner-loader-file/spinner-loader-file.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, Input, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { CreateHostDirective } from '../../directives/create-host.directive';
 import { CreateHostService } from '../../services/create-host.service';
 
@@ -9,16 +9,16 @@ import { CreateHostService } from '../../services/create-host.service';
   selector: 'app-spinner-loader-file',
   template: '<ng-template createHost></ng-template>',
 })
-export class SpinnerLoaderFileComponent implements OnDestroy, AfterViewInit  {
+export class SpinnerLoaderFileComponent implements AfterViewInit  {
 
   @Input() show: boolean = true;
   @Input() percent: number = 0;
   @Input() text: string = "Descargando archivo";
   @ViewChild(CreateHostDirective, { static: true }) createHostDirective: CreateHostDirective;
   constructor(private chs: CreateHostService) { }
-  ngOnDestroy(): void {
-    this.chs.destroyComponent();
-  }
+  // ngOnDestroy(): void {
+  //   // this.chs.destroyComponent();
+  // }
   
   ngAfterViewInit(): void {
     this.chs.setCreateHostDirective(this.createHostDirective);
